9d696df7cb8054c34815d0703d5c8713784157ae
cneil89 <ceneil@oakland.edu>
2019-01-23 13:41:33 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
    console.log('Connected to server');
-
-   socket.emit('createMessage', {
-      from: 'Colin',
-      text: 'Hello'
-   });
 });
 
 socket.on('disconnect', function () {
